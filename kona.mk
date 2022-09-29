01038aec94281a3451fbc1d6a2dc37990b3dea4f
Ritzzz <riteshm321@gmail.com>
2022-09-29 16:18:23 +0000

    sm8250-common: Switch to QTI power hal
    Revert "sm8250-common: Import powerhint.json from android-11.0.0_r32"
    
    This reverts commit 7386edc221263c4ef46b110144a90cdca8d399e2.

--- kona.mk
+++ kona.mk
@@ -355,15 +355,7 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/interfaces \
-    hardware/google/pixel
+    android.hardware.power-service-qti
 
 # Public libraries
 PRODUCT_COPY_FILES += \
