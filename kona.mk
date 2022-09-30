320b3f6c0f242efb9aa29536ffbeaa288f013c6a
Ritzzz <riteshm321@gmail.com>
2022-09-30 13:08:05 +0000

    sm8250-common: Remove Mlipay and Motor sepolicies

--- kona.mk
+++ kona.mk
@@ -298,9 +298,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # Mlipay
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
+#PRODUCT_PACKAGES += \
+#    vendor.xiaomi.hardware.mlipay@1.1.vendor \
+#    vendor.xiaomi.hardware.mtdservice@1.0.vendor
 
 # Net
 PRODUCT_PACKAGES += \
