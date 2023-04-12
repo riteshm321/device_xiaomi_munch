a999ae7be602ba6e4efdee8d5c15fbec4ca5b254
Ritzzz <riteshm321@gmail.com>
2023-04-13 00:12:34 +0530

    treewide: munch: Bringup Bootleggers [1/2]

--- bootleg_munch.mk
+++ bootleg_munch.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 BootleggersROM
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,15 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Bootleggers ROM stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-DEVICE_MAINTAINER := madmax7896
+DEVICE_MAINTAINER := Ritzzz
 
-PRODUCT_NAME := arrow_munch
+PRODUCT_NAME := bootleg_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
